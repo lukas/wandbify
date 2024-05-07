14304ee99028f9bcd9965fe8410dbb21bdf8abad
Lukas Biewald <lukeab@gmail.com>
2024-05-07 11:10:39 -0700

    new files

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,118 @@
+'use client';
 import Image from "next/image";
+import React from 'react';
+import { OpenAI } from "openai";
+import { useState } from "react";
+import { Button } from "flowbite-react";
+import { Label, Textarea, Card, Spinner, Modal } from "flowbite-react"
+
+const openai = new OpenAI();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+openai.apiKey = OPENAI_API_KEY;
+
+let response: string = ''
+const defaultBrandGuidelines = `
+Clear
+
+We show respect to our audience by writing as clearly and concisely as possible. We avoid jargon and flowery language. For example, we will never use a word like “utilize” and will always use the simpler “use”. We avoid long sentences. We avoid the passive voice.
+We assume that words mean what they mean in the common vernacular vs what we wish the words mean or what pedants might declare the words to mean. For example if LLMs (Large Language Models) come to refer to models that also process videos and images we will use the term LLMs in that way.
+We are not afraid to use bullet points instead of paragraphs.
+
+Friendly
+We want our audience to feel like we are human and approachable. We use informal grammar, slang and emojis when it improves the clarity of the communication.
+We never want our audience to feel confused or stupid. We also recognize that a large percent of our readers are not native English speakers. We err on the side of writing out acronyms and explaining or avoiding technical terms that our audience might not know.
+Concrete
+
+Our audience is builders. We avoid lazy business phrases like “delivering value” and use more specific language.
+`
+
+
+
+
+
 
 export default function Home() {
+  const [response, setResponse] = useState('')
+  const [inProgress, setInProgress] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
+  const [brandGuidelines, setBrandGuidelines] = useState(defaultBrandGuidelines);
+
+
+  async function openaiRewrite(text: string) {
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: `Rewrite the text input to adhere to our brand guidlines, which are as follows ${brandGuidelines}` }, { role: "user", content: text }],
+      model: "gpt-3.5-turbo",
+    });
+    setInProgress(false)
+    setResponse(completion.choices[0].message.content || "");
+
+  }
+
+  function handleClick() {
+    const text = document.getElementById('message') as HTMLTextAreaElement;
+    setInProgress(true)
+    openaiRewrite(text.value)
+  }
+
+  function resetBrandGuidelines() {
+    const text = document.getElementById('brandGuidelines') as HTMLTextAreaElement;
+    setBrandGuidelines(text.value)
+    setOpenModal(false)
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex flex-col items-center h-screen p-24">
+
+        <div className="flex grow h-3/4 p-8 w-3/4">
+          <Card className="bg-white grow m-4 p-2 bg-white border border-gray-200 rounded-lg shadow">
+            <h5 className="text-2xl tracking-tight text-gray-900 dark:text-white">Your Text</h5>
+            <Textarea className="flex-1 h-full" id="message" name="message" ></Textarea>
+
+          </Card>
+          <Card className="bg-white grow m-4 p-2 bg-white border border-gray-200 rounded-lg shadow">
+            <h5 className="text-2xl tracking-tight text-gray-900 dark:text-white">Rewritten Text</h5>
+
+            <Textarea className="flex-1 h-full" name="result" id='result' readOnly value={response}>
+            </Textarea>
+
+
+
+          </Card>
+
+        </div >
+        <div className="flex flex-row">
+          <Button onClick={handleClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Rewrite
+            {inProgress ? <Spinner /> : ""}
+
+          </Button>
+          {/*        
+            <Button onClick={() => setOpenModal(true)}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Edit Guidelines
+
+          </Button>  */}
         </div>
       </div>
+      <Modal show={openModal} size="lg" popup position='center'>
+        <Modal.Header>Text Guidelines</Modal.Header>
+        <Modal.Body>
+          <div>
+            <Textarea className="flex-1 h-full" id="brandGuidelines" defaultValue={brandGuidelines}></Textarea>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => resetBrandGuidelines}>Change</Button>
+          <Button color="gray" onClick={() => setOpenModal(false)}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
